a3964a54f7e32aadd1e84002d40d9212d3ffd103
Adil Sikandar <zeeshanhurmat@192.168.10.4>
2020-08-30 23:18:35 +0300

    chat app

---
+++ backend/configs/users.js
@@ -0,0 +1,30 @@
+module.exports =  [
+    {
+        id: 1,
+        name: 'Adil'
+    },
+    {
+        id: 2,
+        name: 'Nadia'
+    },
+    {
+        id: 3,
+        name: 'Faiza'
+    },
+    {
+        id: 4,
+        name: 'Mahnoor'
+    },
+    {
+        id: 5,
+        name: 'CR'
+    },
+    {
+        id: 6,
+        name: 'Nabeel'
+    },
+    {
+        id: 7,
+        name: 'Fatima'
+    }
+]
